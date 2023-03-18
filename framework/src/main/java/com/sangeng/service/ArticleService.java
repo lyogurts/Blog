4b4469f358a3ede457a1db077934a4ab3af07ce9
Lyogurts <lyogurt@163.com>
2023-03-18 11:33:42 +0800

    环境搭建+bean拷贝

--- framework/src/main/java/com/sangeng/service/ArticleService.java
+++ framework/src/main/java/com/sangeng/service/ArticleService.java
@@ -2,6 +2,8 @@ package com.sangeng.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.sangeng.domain.entity.Article;
+import com.sangeng.domain.entity.ResponseResult;
 
 public interface ArticleService extends IService<Article> {
+    ResponseResult hotArticleList();
 }
--- framework/src/main/java/com/sangeng/service/impl/ArticleServiceImpl.java
+++ framework/src/main/java/com/sangeng/service/impl/ArticleServiceImpl.java
@@ -1,12 +1,46 @@
 package com.sangeng.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.sangeng.constants.SystemConstants;
 import com.sangeng.domain.entity.Article;
+import com.sangeng.domain.entity.HotArticleVo;
+import com.sangeng.domain.entity.ResponseResult;
 import com.sangeng.mapper.ArticleMapper;
 import com.sangeng.service.ArticleService;
+import com.sangeng.utils.BeanCopyUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, Article> implements ArticleService {
 
+    @Override
+    public ResponseResult hotArticleList() {
+        LambdaQueryWrapper<Article> queryWrapper = new LambdaQueryWrapper<>();
+        //必须是正式文章
+        queryWrapper.eq(Article::getStatus, SystemConstants.ARTICLE_STATUS_NORMAL);
+        //按照浏览量进行排序
+        queryWrapper.orderByDesc(Article::getViewCount);
+        //最多只查询10条
+        Page<Article> page = new Page(1,10);
+        page(page,queryWrapper);
+        List<Article> articles = page.getRecords();
+        //bean拷贝
+//        ArrayList<HotArticleVo> articleVos = new ArrayList<>();
+//        articles.forEach(value->
+//                {
+//                    HotArticleVo vo = new HotArticleVo();
+//                    BeanUtils.copyProperties(value,vo);
+//                    articleVos.add(vo);
+//                }
+//                );
+        List<HotArticleVo> hotArticleVos = BeanCopyUtils.copyBeanList(articles, HotArticleVo.class);
+
+        return ResponseResult.okResult(hotArticleVos);
+    }
 }
